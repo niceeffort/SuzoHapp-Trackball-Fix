8957aeb10827c64d6c7ede3fbe31645068fb07e1
David Schoemehl <davidschoemehl@gmail.com>
2023-06-06 21:31:14 -0500

    Adding code to output info

--- SuzoHappTrackballFix/SuzoHappTrackballFix/SuzoHappTrackballFix.cpp
+++ SuzoHappTrackballFix/SuzoHappTrackballFix/SuzoHappTrackballFix.cpp
@@ -2,18 +2,19 @@
 #include <chrono>
 #include <iostream>
 #include <fstream>
+#include <queue>
+#include <string>
 #include "interception.h"
 #include "utils.h"
 
 enum ScanCode
 {
-	SCANCODE_ESC = 0X01
+	SCANCODE_ESC = 0X01,
+	SCANCODE_R = 19
 };
 
 int main()
 {
-	//std::ofstream dataFile;
-	//dataFile.open("data.txt");
 
 
 	InterceptionContext context;
@@ -22,7 +23,8 @@ int main()
 
 	context = interception_create_context();
 
-	interception_set_filter(context, interception_is_keyboard, INTERCEPTION_FILTER_KEY_DOWN | INTERCEPTION_FILTER_KEY_UP);
+	//interception_set_filter(context, interception_is_keyboard, INTERCEPTION_FILTER_KEY_DOWN | INTERCEPTION_FILTER_KEY_UP);
+	interception_set_filter(context, interception_is_keyboard, INTERCEPTION_FILTER_KEY_DOWN);
 	interception_set_filter(context, interception_is_mouse, INTERCEPTION_FILTER_MOUSE_MOVE);
 
 	auto lastTimeStamp = std::chrono::system_clock::now();
@@ -31,11 +33,27 @@ int main()
 	int overrideDuration = 60;
 	int maxYMovement = -60;
 	int accelThreshold = -90000;
+	int maxDataPoints = 500;
+	int recCount = 0;
+	int eventNumber = 0;
+
+	struct dataPoint {
+		int m_originalY;
+		int m_modifiedY;
+		int m_lastY;
+		int m_time;
+		double m_accel;
+		dataPoint(int originalY, int modifiedY, int lastY, int time, double accel)
+			: m_originalY(originalY), m_modifiedY(modifiedY), m_lastY(lastY), m_time(time), m_accel(accel)
+		{}
+	};
+	
+	std::queue<dataPoint> q;
 
 	while (interception_receive(context, device = interception_wait(context), &stroke, 1) > 0) {
 
 		if (interception_is_mouse(device)) {
-
+			eventNumber++;
 			InterceptionMouseStroke& mstroke = *(InterceptionMouseStroke*)&stroke;
 
 			int originalYValue = mstroke.y;
@@ -44,6 +62,14 @@ int main()
 
 				auto timeStamp = std::chrono::system_clock::now();
 				auto duration = timeStamp.time_since_epoch();
+
+				std::chrono::duration<double> diffInTimestamps = timeStamp - lastTimeStamp;
+				double diffInTimestampsSquared = pow(diffInTimestamps.count(), 2);
+
+				double accel = 0;
+				if (diffInTimestampsSquared != 0) {
+					accel = (mstroke.y - lastValue) / (diffInTimestampsSquared);
+				}
 
 				if (overrideCount != 0) {
 					mstroke.y = maxYMovement;
@@ -55,14 +81,6 @@ int main()
 				}
 				else
 				{
-					std::chrono::duration<double> diffInTimestamps = timeStamp - lastTimeStamp;
-					double diffInTimestampsSquared = pow(diffInTimestamps.count(), 2);
-
-					double accel = 0;
-					if (diffInTimestampsSquared != 0) {
-						accel = (mstroke.y - lastValue) / (diffInTimestampsSquared);
-					}
-
 					//std::wcout << accel << " ---- " << mstroke.y << ' ' << lastValue << ' ' << ' ' << originalYValue << ' ' << diffInTimestamps.count() << ' ' << std::endl;
 					//dataFile << duration.count() << ',' << originalYValue << ',' << mstroke.y << std::endl;
 					
@@ -73,6 +91,11 @@ int main()
 					}
 				}
 
+				if (q.size() == maxDataPoints) {
+					q.pop();
+				}
+				q.push(dataPoint(originalYValue, mstroke.y, lastValue, eventNumber, accel));
+
 				lastTimeStamp = timeStamp;
 				lastValue = mstroke.y;
 
@@ -82,15 +105,25 @@ int main()
 
 		if (interception_is_keyboard(device)) {
 			InterceptionKeyStroke& kstroke = *(InterceptionKeyStroke*)&stroke;
-
-			interception_send(context, device, &stroke, 1);
-
+			std::wcout << "keyboard" << kstroke.code << std::endl;
+			/*
+			if (kstroke.code == SCANCODE_R) {
+				std::ofstream dataFile;
+				recCount++;
+				std::string filename = "data" + std::to_string(recCount) + ".txt";
+				dataFile.open(filename);
+				while (q.size() > 0) {
+					dataPoint point = q.front();
+					dataFile << point.m_time << ',' << point.m_lastY << ',' << point.m_originalY << ',' << point.m_modifiedY << ',' << point.m_accel << std::endl;
+					q.pop();
+				}
+				dataFile.close();
+			}
 			//if (kstroke.code == SCANCODE_ESC) break;
+			*/
 		}
 	}
 
 	interception_destroy_context(context);
-	//dataFile.close();
-
 	return 0;
 }
